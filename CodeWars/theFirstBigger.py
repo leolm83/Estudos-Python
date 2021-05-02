3e64a1c123ebb0cf47c54efe3ee11996d9f94288
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-05-02 11:12:05 -0300

    Update theFirstBigger.py

--- CodeWars/theFirstBigger.py
+++ CodeWars/theFirstBigger.py
@@ -10,3 +10,15 @@ def longest_consec(arr, k):
                 theFirstBigger=aux
     return theFirstBigger
 ##https://www.codewars.com/kata/56a5d994ac971f1ac500003e/train/python
+
+###outra solucao
+def longest_consec(strarr, k):
+    result = ""
+    
+    if k > 0 and len(strarr) >= k:
+        for index in range(len(strarr) - k + 1):
+            s = ''.join(strarr[index:index+k])
+            if len(s) > len(result):
+                result = s
+            
+    return result
