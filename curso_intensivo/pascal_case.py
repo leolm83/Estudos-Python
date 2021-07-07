8af4d2971026a513e64de57350312d2f92326f6e
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-07 13:12:22 -0300

    Create pascal_case.py
    Programa usando a funcao title() que deixa as strings no formato PascalCase

---
+++ curso_intensivo/pascal_case.py
@@ -0,0 +1,7 @@
+primeiro_nome="leonardo"
+ultimo_nome="marques"
+saudacao="Olá , "
+print(saudacao+"\n\t"+(primeiro_nome+" "+ultimo_nome).title())
+###    \t dá uma tabulacao
+###    \n pula uma linha
+###    "qualquer string".title() retorna as palavras com a Primeira Letra Maiuscula(ou seja PascalCase)
