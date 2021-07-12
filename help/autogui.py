f89c5953c7ce5c6201b7225eea3898c479a28c78
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-12 19:25:02 -0300

    Update autogui.py

--- help/autogui.py
+++ help/autogui.py
@@ -1,5 +1,4 @@
-Leonardo Mateus
-Nao testei e nem sei como funciona o py auto gui mas testa pra mim pfv contador = 0
+contador = 0
 teclado = pyautogui.press ('down')
 while contador < 100
   pyautogui.click(2, 22)
