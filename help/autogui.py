b032b20b85ef9b56bcd8b1d471bb6811b76589f1
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-12 19:24:01 -0300

    Create autogui.py

---
+++ help/autogui.py
@@ -0,0 +1,8 @@
+Leonardo Mateus
+Nao testei e nem sei como funciona o py auto gui mas testa pra mim pfv contador = 0
+teclado = pyautogui.press ('down')
+while contador < 100
+  pyautogui.click(2, 22)
+  for x in range (contador):
+    teclado()
+  contador = contador + 1
