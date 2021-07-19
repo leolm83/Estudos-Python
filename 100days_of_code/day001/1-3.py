ae7874498d9349a7c71ba9e970065b8a3d620b1c
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-19 19:06:24 -0300

    Input function

---
+++ 100days_of_code/day001/1-3.py
@@ -0,0 +1,8 @@
+#DICA 1
+# CTRL + / comenta uma linha de codigo
+# what happens above:
+# input() will get user input in console
+# then print() will print the word "Hello" and the user input
+print("Hello "+input("Whats Your Name ? "))
+#EXERCISE 1.3 Input Function
+print("Seu nome tem : "+str(len(input("Qual é o seu nome? ")))+" caracteres")
