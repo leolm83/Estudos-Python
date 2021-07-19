7a43a438775e6f1f3ba19257fd90db7b1377efeb
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-19 19:33:52 -0300

    primitive data types

---
+++ 100days_of_code/day002/2-0.py
@@ -0,0 +1,23 @@
+#primitive data types
+#Strings
+#"UMA STRING"
+#é possivel acessar os caracteres de  uma string a partir dos indices
+#"UMA STRING"[0] retorna o valor U
+#"0123456789" 
+
+
+#Integer
+# a=32
+# voce pode usar o Underscore para facilitar a leitura de casas em numeros que seriam no mundo real
+#representados como 132.000,00 ou 100.000.000,00 no python sao:
+# b = 132_000
+# c = 100_000_000
+
+#Floats
+# x = 40.00
+# y = 500_000.00 REPRESENTA 500.000,00
+# z = 0.8
+
+#Boolean
+#True
+#False
