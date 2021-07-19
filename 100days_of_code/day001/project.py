05f128f4b918f66b9151213a87fd74c2f84c713b
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-19 19:08:34 -0300

    gerador de nome de bandas musicais

---
+++ 100days_of_code/day001/project.py
@@ -0,0 +1,6 @@
+#PROJECT DAY 1
+print("\tGerador de Nome de Banda Musical ")
+cidade_natal=input("Em que cidade você nasceu ? ")
+animal_estimacao=input("Qual o nome de seu animal de estimação ? ")
+nome_banda=cidade_natal+" "+animal_estimacao
+print("O nome da sua banda será : "+nome_banda)
