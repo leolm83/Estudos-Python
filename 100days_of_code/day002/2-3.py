697e652f303c886c7a399cc3b8280015ce88bc3e
Leonardo Mateus <59545135+leolm83@users.noreply.github.com>
2021-07-20 12:56:57 -0300

    Create 2-3.py
    Arredondamento e conversoes de inteiros e floats

---
+++ 100days_of_code/day002/2-3.py
@@ -0,0 +1,10 @@
+#EXERCISE 2.3
+# ARREDONDANDO PRECISAMENTE
+print("Ao converter para inteiro se perde as casas decimais")
+print(f"Valor em Float : {8/3}")
+print(f"Valor arredondado para o valor inteiro abaixo (floor) {8//3}")
+print(f"Valor convertido diretamente para Int : {int(8/3)}")
+print(f"Valor Arredondado sem especificar numero de casas decimais : {round(8/3)}")
+print(f"Valor arredondado com 1 casa decimal {round(8/3,1)}")
+print(f"Valor arredondado com 2 casas decimais {round(8/3,2)}")
+print(f"Valor arredondado com 3 casas decimais {round(8/3,3)}")
